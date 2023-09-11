ad628f29d21e29a857a9bd383e08dd60df840d70
Oskar <oskarpasko@gmail.com>
2023-09-11 11:09:56 +0200

    create project with two files
    testing project
    - test two files with classes

---
+++ csharp-classes/TwoFilesClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwoFilesClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoginButton loginButton = new LoginButton();
+            loginButton.create();
+            loginButton.action();
+        }
+    }
+}
