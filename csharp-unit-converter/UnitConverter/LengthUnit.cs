20133534fd83762ef345668df823d43315d7e9e2
Oskar <oskarpasko@gmail.com>
2023-09-15 10:35:03 +0200

    change some variables names  and update menu

--- csharp-unit-converter/UnitConverter/LengthUnit.cs
+++ csharp-unit-converter/UnitConverter/LengthUnit.cs
@@ -8,37 +8,37 @@ namespace UnitConverter
 {
         public class LengthUnit : Unit
     {
-        private double number;
+        private double value;
 
         public override void get_number()
         {
             // enter a value to convert
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("\nEnter a number to convert:", Console.ForegroundColor);
-                this.number = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nEnter a value to convert:", Console.ForegroundColor);
+                this.value = Convert.ToDouble(Console.ReadLine());
         }
 
         public override int get_unit()
         {
-            // enter a number's unit from what we will convert
+            // enter a value's unit from what we will convert
                 Console.WriteLine("\nWhat unit did You enter the value?");
-                Console.WriteLine("1. mm");
-                Console.WriteLine("2. cm");
-                Console.WriteLine("3. dm");
-                Console.WriteLine("4. m");
-                Console.WriteLine("5. km");
+                Console.WriteLine("1. millimeter (mm)");
+                Console.WriteLine("2. centimeter (cm)");
+                Console.WriteLine("3. decimeter (dm)");
+                Console.WriteLine("4. meter (m)");
+                Console.WriteLine("5. kilometer (km)");
                 int choice = Convert.ToInt32(Console.ReadLine());
 
                 return choice;
         }
 
-        public override void write_result(double nr, double[] results, string[] resultsUnit)
+        public override void write_result(double v, double[] results, string[] resultsUnit)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nResults:", Console.ForegroundColor);
             for(int i = 0; i<5 ; i++)
             {
-                Console.WriteLine($"\n{nr} mm = {results[i]} {resultsUnit[i]}");
+                Console.WriteLine($"\n{v} mm = {results[i]} {resultsUnit[i]}");
             }
         }
 
@@ -54,44 +54,44 @@ namespace UnitConverter
             {
                 case 1: // for mm
                     // create an array with results
-                    result = new double[] {(number), (number/10), 
-                    (number/100), (number/1000), (number/10000000)};
+                    result = new double[] {(value), (value/10), 
+                    (value/100), (value/1000), (value/10000000)};
                     
                     // output results
-                    write_result(number, result, unitName);
+                    write_result(value, result, unitName);
                     break;
                 case 2: // for cm
                     // create an array with results
-                    result = new double[] {(number*10), (number), 
-                    (number/10), (number/100), (number/1000000)};
+                    result = new double[] {(value*10), (value), 
+                    (value/10), (value/100), (value/1000000)};
                     
                     // output results
-                    write_result(number, result, unitName);
+                    write_result(value, result, unitName);
                     break;
                 case 3:
                     // create an array with results
-                    result = new double[] {(number*100), (number*10), 
-                    (number), (number/10), (number/10000)};
+                    result = new double[] {(value*100), (value*10), 
+                    (value), (value/10), (value/10000)};
                     
                     // output results
-                    write_result(number, result, unitName);
+                    write_result(value, result, unitName);
                     break;
                 case 4: // for meter
 
                     // create an array with results
-                    result = new double[]{(number * 1000), (number * 100), 
-                    (number * 10), number, (number / 1000)};
+                    result = new double[]{(value * 1000), (value * 100), 
+                    (value * 10), value, (value / 1000)};
                     
                     // output results
-                    write_result(number, result, unitName);
+                    write_result(value, result, unitName);
                     break;
                 case 5: // for km
                     // create an array with results
-                    result = new double[] {(number*1000000), (number*100000), 
-                    (number*10000), (number*1000), (number)};
+                    result = new double[] {(value*1000000), (value*100000), 
+                    (value*10000), (value*1000), (value)};
                     
                     // output results
-                    write_result(number, result, unitName);
+                    write_result(value, result, unitName);
                     break;
             }
         }
